d6d891e76c3cb2ef6d3f8a8cb70859ee8e276366
damccoy <dmccoy14@fau.edu>
2016-03-10 00:11:33 -0500

    Added files via upload

---
+++ Ipsum (1).java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragments;
+
+import android.widget.ImageView;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+            "Heart Rate Education",
+            "Blood Oxygen Education",
+            "Symptoms of Heat Failure"
+
+    };
+
+    static String[] Articles = {
+            "Heart Rate Education\n\nThe normal heart for children 10 years and older, and adults (including seniors) is 60 - 100 beats per minute. Above 100 BPM is considered high if you are not currently exercising.",
+            "Blood Oxygen Education\n\nSP02 is a percentage that reflects the level of oxygen available in your blood. Your SpO2 is considered to be normal when it is above 95%. If it is around or under 90% your blood contains an abnormally low level of oxygen and it is recommend you talk about it with a health care provider.",
+            "Symptoms of Heart Failure\n\n1. Buildup of fluid in chest or ankles\n2. Sudden weight gain, possibly from fluid build up\n3.Enlargement of the heart chambers"
+    };
+
+    static ImageView[] Pictures = {
+
+    };
+}
